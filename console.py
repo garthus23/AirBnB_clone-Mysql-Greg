9208760e1c744d7ff4a7a7a18621d16fa0cae77a
Gregory ARTHUS <garthus@adminux.fr>
2021-03-29 16:23:48 +0200

    add(): task 6

--- console.py
+++ console.py
@@ -3,6 +3,7 @@
 import cmd
 import sys
 from models.base_model import BaseModel
+from models.db_storage import DBStorage
 from models.__init__ import storage
 from models.user import User
 from models.place import Place
---
+++ env_vars.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+HBNB_MYSQL_USER=root
+HBNB_MYSQL_PWD='root'
+HBNB_MYSQL_HOST=localhost
+HBNB_MYSQL_DB=db
