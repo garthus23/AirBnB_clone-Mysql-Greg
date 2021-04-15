32d730079f049584f07057db3c0790ca4c459682
Gregory <garthus@adminux.fr>
2021-04-15 03:42:42 +0200

    task 3

---
+++ 3-deploy_web_static.py
@@ -0,0 +1,46 @@
+#!/usr/bin/python3
+# do pack and do deploy
+''' module to execute cmd '''
+from fabric.api import *
+
+env.hosts = ['34.75.62.192', '35.196.192.33', ]
+
+
+def deploy():
+    ''' deploy content '''
+    def do_pack():
+        ''' create archive '''
+        try:
+            local("ls | grep -c versions", capture=False)
+        except:
+            local('mkdir versions', capture=False)
+        try:
+            local("tar -cvzf versions/web_static_$(date +%Y%m%d%H%M%S).tgz \
+                  web_static")
+            archive_path = local("ls -t versions | head -n 1")
+            return archive_path
+        except:
+            return None
+
+    def do_deploy(archive_path):
+        ''' upload archive, uncompress '''
+        try:
+            put(archive_path, '/tmp')
+            # file name no extension
+            fine = run("ls -t /tmp | head -n 1 | sed 's/\..*//'")
+            run("mkdir -p /data/web_static/releases/%s" % (fine))
+            # file name with extension
+            fie = run("ls /tmp | grep .tgz")
+            run("tar -xzf /tmp/%s -C \
+                /data/web_static/releases/%s" % (fie, fine))
+            run("rm -f /tmp/%s" % (fie))
+            run("mv /data/web_static/releases/%s//web_static/* \
+                /data/web_static/releases/%s" % (fine, fine))
+            run("rm -rf /data/web_static/releases/%s/web_static" % (fine))
+            run("rm -rf /data/web_static/current")
+            run("ln -s /data/web_static/releases/%s/ \
+                /data/web_static/current" % (fine))
+        except:
+            return False
+
+    do_deploy(do_pack())
