a1356fde26c44f325616c40cf3ea4ac00090415e
Gregory <garthus@adminux.fr>
2021-04-15 18:00:04 +0200

    debug(): correct return value on success

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -24,5 +24,6 @@ def do_deploy(archive_path):
         run("rm -rf /data/web_static/current")
         run("ln -s /data/web_static/releases/%s/ \
             /data/web_static/current" % (fine))
+        return True
     except:
         return False
