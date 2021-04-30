9ee069cc0e1cd0a7b39484824e6aa8465574f5c0
Gregory <garthus@adminux.fr>
2021-05-01 00:35:11 +0200

    style(): doc

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -9,7 +9,7 @@ app = Flask(__name__)
 
 @app.route('/', strict_slashes=False)
 def index():
-""" define a return msg """
+    """ define a return msg """
     return "Hello HBNB!"
 
 if __name__ == "__main__":
