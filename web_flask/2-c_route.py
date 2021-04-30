d59f68cb35061d3eb1f86fb1ce6825ed0c1ad817
Gregory <garthus@adminux.fr>
2021-05-01 00:39:22 +0200

    style(): doc

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -1,20 +1,29 @@
 #!/usr/bin/python3
 
+
+""" flask module """
 from flask import Flask
 
 app = Flask(__name__)
 
+
 @app.route('/', strict_slashes=False)
 def index0():
+    """ index 0 """
     return "Hello HBNB!"
+
 
 @app.route('/hbnb', strict_slashes=False)
 def index1():
+    """ index 1 """
     return "HBNB"
+
 
 @app.route('/c/<text>', strict_slashes=False)
 def cvalue(text):
+    """ cvalue """
     return "C %s" % text
+
 
 if __name__ == "__main__":
     app.run()
