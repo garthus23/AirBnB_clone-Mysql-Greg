380c05c276d0e8498d0bda1476f3f4ab5b20bc72
Gregory <garthus@adminux.fr>
2021-05-01 00:37:18 +0200

    style(): doc

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -1,15 +1,20 @@
 #!/usr/bin/python3
 
+""" module Flask """
 from flask import Flask
 
 app = Flask(__name__)
 
+
 @app.route('/', strict_slashes=False)
 def index0():
+    """ index 0 """
     return "Hello HBNB!"
+
 
 @app.route('/hbnb', strict_slashes=False)
 def index1():
+    """ index 1 """
     return "HBNB"
 
 if __name__ == "__main__":
