089f9f465261bc7030b602e8e962eff64ddbf527
Gregory <garthus@adminux.fr>
2021-05-01 01:00:51 +0200

    style(): doc

--- web_flask/4-number_route.py
+++ web_flask/4-number_route.py
@@ -1,32 +1,48 @@
 #!/usr/bin/python3
 
+
+""" module flask tout mou """
 from flask import Flask
 
 app = Flask(__name__)
 
+
 @app.route('/', strict_slashes=False)
 def index0():
+    """ index 0 """
     return "Hello HBNB!"
+
 
 @app.route('/hbnb', strict_slashes=False)
 def index1():
+    """ index 1 """
     return "HBNB"
+
 
 @app.route('/c/<text>', strict_slashes=False)
 def cvalue(text):
+    """ cvalue """
+    text = text.replace('_', ' ')
     return "C %s" % text
+
 
 @app.route('/python', strict_slashes=False)
 def pyvalue():
+    """ pyvalue """
     return "Python is cool"
+
 
 @app.route('/python/<text>', strict_slashes=False)
 def pytxtvalue(text):
+    """ pytxtvalue """
+    text = text.replace('_', ' ')
     return 'Python is %s' % text
 
 @app.route('/number/<int:n>', strict_slashes=False)
 def isnumber(n):
+    """ isnumber """ 
     return "%d is number" % n
+
 
 if __name__ == "__main__":
     app.run()
