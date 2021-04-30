f76774edf0352d020a31e3c653b9c67b07bfc0d2
Gregory <garthus@adminux.fr>
2021-05-01 01:16:08 +0200

    style() : doc

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -1,36 +1,56 @@
 #!/usr/bin/python3
 
+
+""" module tout mou """
 from flask import Flask, render_template
+
 
 app = Flask(__name__)
 
+
 @app.route('/', strict_slashes=False)
 def index0():
+    """ index 0 """
     return "Hello HBNB!"
+
 
 @app.route('/hbnb', strict_slashes=False)
 def index1():
+    """ index 1 """
     return "HBNB"
+
 
 @app.route('/c/<text>', strict_slashes=False)
 def cvalue(text):
+    """ cvalue """
+    text = text.replace('_', ' ')
     return "C %s" % text
+
 
 @app.route('/python', strict_slashes=False)
 def pyvalue():
+    """ pyvalue """
     return "Python is cool"
+
 
 @app.route('/python/<text>', strict_slashes=False)
 def pytxtvalue(text):
-    return 'Python is %s' % text
+    """ pytxtvalue """
+    text = text.replace('_', ' ')
+    return 'Python %s' % text
+
 
 @app.route('/number/<int:n>', strict_slashes=False)
 def isnumber(n):
-    return "%d is number" % n
+    """ is number """
+    return "%d is a number" % n
+
 
 @app.route('/number_template/<int:n>', strict_slashes=False)
 def dispnpage(n):
+    """ dispnpage """
     return render_template("5-number.html", value=n)
+
 
 if __name__ == "__main__":
     app.run()
