797f7065d480ad38a3885f54a6e74c067348308c
Gregory <garthus@adminux.fr>
2021-05-01 00:31:47 +0200

    style(): pep8

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,11 +1,15 @@
 #!/usr/bin/python3
+# flask app basic
 
+""" module flask """
 from flask import Flask
 
 app = Flask(__name__)
 
+
 @app.route('/', strict_slashes=False)
 def index():
+""" define a return msg """
     return "Hello HBNB!"
 
 if __name__ == "__main__":
