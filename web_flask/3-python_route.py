3c6ea58afbea5a6cf638c4136004d4b63f25cf7c
Gregory <garthus@adminux.fr>
2021-05-01 00:51:55 +0200

    style(): doc

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -1,27 +1,39 @@
 #!/usr/bin/python3
 
+""" module flask tout mou """
 from flask import Flask
 
 app = Flask(__name__)
 
+
 @app.route('/', strict_slashes=False)
 def index0():
+    """ index 0 """
     return "Hello HBNB!"
+
 
 @app.route('/hbnb', strict_slashes=False)
 def index1():
+    """ index 1 """
     return "HBNB"
+
 
 @app.route('/c/<text>', strict_slashes=False)
 def cvalue(text):
+    """ c value text """
+    text = text.replace('_', ' ')
     return "C %s" % text
+
 
 @app.route('/python', strict_slashes=False)
 def pyvalue():
+    """ pyvalue """
     return "Python is cool"
+
 
 @app.route('/python/<text>', strict_slashes=False)
 def pytxtvalue(text):
+    """ pytxtvalue """
     return 'Python is %s' % text
 
 if __name__ == "__main__":
